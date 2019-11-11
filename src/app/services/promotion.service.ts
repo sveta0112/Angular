894c53266e142d04ad9c3590dc55f553ac007e4e
sveta0112 <sveta0112@hotmail.com>
2019-11-10 22:08:12 -0500

    promotion

--- src/app/services/promotion.service.ts
+++ src/app/services/promotion.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
-
+//
 @Injectable({
   providedIn: 'root'
 })
